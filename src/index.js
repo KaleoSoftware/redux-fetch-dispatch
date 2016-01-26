9d97a0afd7ca75fe82a5eb1f071132b9aabdee12
Tucker Connelly <web@tuckerconnelly.com>
2016-01-25 21:29:58 -0500

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import fetch from 'isomorphic-fetch'
 
 export default function fetchDispatch(url, options = {}, actions = {}) {
-	// If there are only two parameters, the second one is actions
-	if (actions === {}) {
-		actions = options
-		options = undefined
-	}
+  // If there are only two parameters, the second one is actions
+  if (actions === {}) {
+    actions = options
+    options = undefined
+  }
 
-	return (dispatch) => {
+  return (dispatch) => {
 
-		if (typeof actions.request === 'function')  {
-			actions.request()
-		}
+    if (typeof actions.request === 'function')  {
+      actions.request()
+    }
 
-		return fetch(url, options)
-			.then( (response) => {
-				if (response.status.toString().charAt(0) === '2') {
-					return response.json()
-				} else if (response.status.toString().charAt(0) === '4') {
-					throw response.json()
-				}
-			})
-			.then( (json) => {
-				if (typeof actions.success === 'function') {
-					dispatch(actions.success(json))
-				}
-				return json
-			})
-			.catch( (err) => {
-				if (typeof actions.fail === 'function') {
-					dispatch(actions.fail(err))
-				}
-				return err
-			})
-	}
+    return fetch(url, options)
+      .then( (response) => {
+        if (response.status.toString().charAt(0) === '2') {
+          return response.json()
+        } else if (response.status.toString().charAt(0) === '4') {
+          throw response.json()
+        }
+      })
+      .then( (json) => {
+        if (typeof actions.success === 'function') {
+          dispatch(actions.success(json))
+        }
+        return json
+      })
+      .catch( (err) => {
+        if (typeof actions.fail === 'function') {
+          dispatch(actions.fail(err))
+        }
+        return err
+      })
+  }
 }
